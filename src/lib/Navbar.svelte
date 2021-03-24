d51af7c48b5f1ebaaad6087278b5f3f76765cdad
Manuel Serret <manuel-serret@t-online.de>
2021-03-24 16:58:28 +0100

    Add pages

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,7 +1,3 @@
-<script>
-    export let application;
-</script>
-
 <nav class="navbar" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
         <a class="navbar-item" href="https://quote-maker.serret.dev">
